683f28ade3d5d87648a355caed3bf0a9210ef5d4
Siddeshahp <siddeshsid99@gmail.com>
2022-01-31 23:35:17 +0530

    commit

---
+++ Mphasis_Hibernate/src/com/simplilearn/entity/EProduct.java
@@ -0,0 +1,52 @@
+package com.simplilearn.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class EProduct {
+
+	@Id
+	@GeneratedValue
+	@Column(name="product_id")
+	private int product_id;
+	
+	@Column(name="product_name")
+	private String product_name;
+	
+	@Column(name="product_price")
+	private int product_price;
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+
+	public int getProduct_price() {
+		return product_price;
+	}
+
+	public void setProduct_price(int product_price) {
+		this.product_price = product_price;
+	}
+
+	
+	//ALt + S + R
+	
+	
+}
